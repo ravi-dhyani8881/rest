5b9ec4452af94f66b6de65eacee8d88a459f17ea
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2025-12-07 04:13:26 +0000

    Automated commit by Docker container

--- ravisuborg1/ravidevenv/9dd0c8bf-0a27-4293-9442-db5b86a7c07f/FEBE8/src/main/java/com/spring/rest/util/JwtFilter.java
+++ ravisuborg1/ravidevenv/9dd0c8bf-0a27-4293-9442-db5b86a7c07f/FEBE8/src/main/java/com/spring/rest/util/JwtFilter.java
@@ -1,41 +1,53 @@
 package com.spring.rest.util;
 
+import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired
-    JwtUtil jwtUtil;
+    private JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException, java.io.IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
             String token = authHeader.substring(7);
 
             try {
                 Claims claims = jwtUtil.extractAllClaims(token);
-                request.setAttribute("userId", claims.getSubject());
-                request.setAttribute("email", claims.get("email"));
+
+                // Create authentication object
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                claims.getSubject(), null, null
+                        );
+
+                // Mark request as authenticated
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
             } catch (Exception e) {
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 return;
             }
         }
+
         chain.doFilter(request, response);
     }
 }
