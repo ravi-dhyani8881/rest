e5e41c4dd9d20be4c3182bd856e4d67aaab4ad29
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2025-12-07 17:08:11 +0000

    Automated commit by Docker container

--- ravisuborg1/ravidevenv/9dd0c8bf-0a27-4293-9442-db5b86a7c07f/FEBE8/src/main/java/com/spring/rest/SwaggerConfig.java
+++ ravisuborg1/ravidevenv/9dd0c8bf-0a27-4293-9442-db5b86a7c07f/FEBE8/src/main/java/com/spring/rest/SwaggerConfig.java
@@ -1,38 +1,25 @@
 package com.spring.rest;
 
-
-
-import java.util.ArrayList;
-
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-
-@Component
+@Configuration
 public class SwaggerConfig {
-	
-	@Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("com.spring.ratting.control"))
-                .build();
-    }
-	
-	private ApiInfo metaData() {
-        ApiInfo apiInfo = new ApiInfo(
-                "Reviews Ratting REST API",
-                "Reviews Ratting API for Online Store",
-                "1.0",
-                "Terms of service",
-                new Contact("Ravi dhyani", "https://springframework.guru/about/", "ravi.dhyani8881@gmail.com"),
-               "Apache License Version 2.0",
-                "https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
-        return apiInfo;
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
