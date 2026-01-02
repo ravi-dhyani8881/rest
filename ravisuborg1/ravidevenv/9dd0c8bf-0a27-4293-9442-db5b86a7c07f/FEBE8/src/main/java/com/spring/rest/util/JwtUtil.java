4cf2a656b635ba36250d2555450968bcb573ccdb
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2026-01-02 06:33:15 +0000

    Automated commit by Docker container

--- ravisuborg1/ravidevenv/9dd0c8bf-0a27-4293-9442-db5b86a7c07f/FEBE8/src/main/java/com/spring/rest/util/JwtUtil.java
+++ ravisuborg1/ravidevenv/9dd0c8bf-0a27-4293-9442-db5b86a7c07f/FEBE8/src/main/java/com/spring/rest/util/JwtUtil.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+import com.spring.rest.model.User;
 
 import java.security.Key;
 import java.util.Date;
