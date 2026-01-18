785b75cfcd0d08801ca90be965c69f13e354b163
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2026-01-18 07:05:55 +0000

    Automated commit by Docker container

---
+++ ravisuborg1/ravidevenv/9dd0c8bf-0a27-4293-9442-db5b86a7c07f/FEBElocalrest/src/main/java/com/spring/rest/model/Invitation.java
@@ -0,0 +1,109 @@
+package com.spring.rest.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data // Generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor // Generates a no-argument constructor (required by Jackson for deserialization)
+@AllArgsConstructor // Generates a constructor with all fields
+@JsonInclude(JsonInclude.Include.NON_NULL) 
+public class Invitation implements Serializable {
+
+
+    @JsonProperty("ID")
+    private String iD;
+
+    private String email;
+
+    private String orgId;
+
+    private String role;
+
+    private String token;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+
+/*
+
+    @Id
+    private String iD;
+    private String email;
+    private String orgId;
+    private String role;
+    private String token;
+    private String createdAt;
+    private String updatedAt;
+
+*/
+
+    @JsonProperty("ID")
+    public String getID() {
+        return iD;
+    }
+
+    @JsonProperty("ID")
+    public void setID( String iD) {
+        this.iD = iD;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail( String email) {
+        this.email = email;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId( String orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole( String role) {
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken( String token) {
+        this.token = token;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt( String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt( String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
