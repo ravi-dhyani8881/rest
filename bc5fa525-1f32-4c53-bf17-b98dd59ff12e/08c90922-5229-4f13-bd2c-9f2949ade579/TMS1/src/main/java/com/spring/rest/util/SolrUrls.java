89ea3c502c24434fc722c7dbed44a4348119563b
ravi-dhyani8881 <ravi.dhyani@mulesoft.com>
2026-01-21 14:17:50 +0000

    Automated commit by Docker container

---
+++ bc5fa525-1f32-4c53-bf17-b98dd59ff12e/08c90922-5229-4f13-bd2c-9f2949ade579/TMS1/src/main/java/com/spring/rest/util/SolrUrls.java
@@ -0,0 +1,41 @@
+/*
+
+package com.spring.rest.util;
+
+public interface SolrUrls {
+	public static String protocol="http";
+			
+	public static String host="solr-service-d1ffe9c3-66db-4da6-85da-6caf5b01f28c";
+			
+	
+    public static String port="80";
+	public static String context="solr";
+							
+      public static String baseUrl= protocol+"://"+host+":"+port+"/"+context;
+	
+  //  public static String APIKEY_URL= protocol+"://"+host+":"+port+"/"+context+"/apiKey";
+
+
+    public static String APIKEY_URL = protocol + "://" + host + ":" + port + "/" + context + "/apiKey";
+
+    public static final String USERS_URL = baseUrl+"/"+"users";
+}
+
+*/
+
+
+package com.spring.rest.util;
+
+public interface SolrUrls {
+    public static String protocol = "http";
+    public static String host = "solr-service-d1ffe9c3-66db-4da6-85da-6caf5b01f28c";
+    public static String port = "80";
+    public static String context = "solr";
+							
+    public static String baseUrl = protocol + "://" + host + ":" + port + "/" + context;
+
+    public static String APIKEY_URL = protocol + "://" + host + ":" + port + "/" + context + "/apiKey";
+
+    public static final String USERS_URL = baseUrl + "/" + "users";
+}
+
